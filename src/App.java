c5d9f7cbf05ea222666501be433644df0be0de71
Emil Jørgensen <48445971+Emilioso007@users.noreply.github.com>
2023-11-11 19:58:29 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import processing.core.*;
+
+public class App extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("App");
+    }
+
+    public void settings() {
+        size(600, 400, P2D);
+    }
+
+    public void draw() {
+        background(42);
+    }
+
+}
